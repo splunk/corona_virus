02e28af8896c086e075c39e31b2e4c8e1de7b979
Don Slife <dslife@splunk.com>
2020-05-09 12:09:19 -0400

    Redirected std error to std out for the git invocations since git wants to write information like
    From https://github.com/CSSEGISandData/COVID-19
     * branch              master     -> FETCH_HEAD
    
    to stdout.
    
    The net result is to remove 2 error lines from _internal every time
    the script is executed, as well as put the two lines into
    the update_git soucetype so the users can see everythign that happened.

--- bin/update_git.sh
+++ bin/update_git.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd $SPLUNK_HOME/etc/apps/corona_virus/git/COVID-19/
-git checkout master
+git checkout master 2>&1
 cd $SPLUNK_HOME/etc/apps/corona_virus
-git submodule foreach git pull origin master
+git submodule foreach git pull origin master 2>&1
