83fa6151ea1dad1a6ff3f5fa5ab101de47b7360c
Don Slife <dslife@splunk.com>
2020-05-10 11:53:10 -0400

    Added a checout of main to the NYC dataset just like the main COVID data set.  This way we don't get a bunch of logging the first time the scripted input runs.

--- bin/update_git.sh
+++ bin/update_git.sh
@@ -1,4 +1,12 @@
+#!/bin/sh
+# Update the sub repos to the current head of the repo.  This isn't
+# necessary, but it stops a bunch of confusing messages going into
+# main the first time the submodules are updated from the remote
 cd $SPLUNK_HOME/etc/apps/corona_virus/git/COVID-19/
 git checkout master
+cd $SPLUNK_HOME/etc/apps/corona_virus/git/NYState-COVID-19-Tracker/
+git checkout master
+
+# Here is where we actually upll updates from the submodule data sources.
 cd $SPLUNK_HOME/etc/apps/corona_virus
 git submodule foreach git pull origin master
